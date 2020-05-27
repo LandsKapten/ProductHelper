5d594d2dd93d8b25fef47a1a0fe66c35ea8fde88
LandsKapten <65979495+LandsKapten@users.noreply.github.com>
2020-05-27 17:53:48 +0200

    Merge branch 'master' of https://github.com/LandsKapten/ProductHelper
    # Conflicts:
    #	app/update.xml

--- app/src/main/java/com/example/mplproducthelperv20/SecondActivity.java
+++ app/src/main/java/com/example/mplproducthelperv20/SecondActivity.java
@@ -14,14 +14,37 @@ package com.example.mplproducthelperv20;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private void AppUpdater() {
+        AppUpdater appUpdater = new AppUpdater(this);
+        appUpdater.setDisplay(Display.DIALOG);
+        appUpdater.setUpdateFrom(UpdateFrom.XML);
+        appUpdater.setUpdateJSON("https://raw.githubusercontent.com/LandsKapten/ProductHelper/master/app/update.xml");
+        appUpdater.setTitleOnUpdateAvailable("Update available");
+        appUpdater.setTitleOnUpdateNotAvailable("Update not available");
+        appUpdater.setContentOnUpdateNotAvailable("No update available. Check for updates again later!");
+        appUpdater.setButtonUpdate("Update now");
+        appUpdater.setButtonDoNotShowAgain(null);
+        appUpdater.setCancelable(false);
+        appUpdater.showAppUpdated(true);
+        appUpdater.start();
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        TextView tvVersion = (TextView) findViewById(R.id.tvAppVersion);
+        tvVersion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppUpdater();
+            }
+        });
+
         Button coalButton = (Button)findViewById(R.id.coalBtn);
         coalButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SecondActivity.this, coal.class));
