626abca55b0993b79b385fd3c5a9353de1834a3b
LandsKapten <65979495+LandsKapten@users.noreply.github.com>
2020-05-27 15:59:03 +0200

    Add Update.xml and reorginizing

--- app/src/main/java/com/example/mplproducthelperv20/MainActivity.java
+++ app/src/main/java/com/example/mplproducthelperv20/MainActivity.java
@@ -30,28 +30,38 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    private void AppUpdater(){
         AppUpdater appUpdater = new AppUpdater(this);
         appUpdater.setDisplay(Display.DIALOG);
         appUpdater.setUpdateFrom(UpdateFrom.XML);
-        appUpdater.setUpdateXML("https://raw.githubusercontent.com/LandsKapten/ProductHelper/master/app/update.xml");
-
-       // appUpdater.setUpdateFrom(UpdateFrom.JSON);
-       // appUpdater.setUpdateJSON("https://raw.githubusercontent.com/LandsKapten/ProductHelper/master/app/update-changelog.json");
+        appUpdater.setUpdateJSON("https://raw.githubusercontent.com/LandsKapten/ProductHelper/master/app/update.xml");
         appUpdater.setTitleOnUpdateAvailable("Update available");
-        appUpdater.setContentOnUpdateAvailable("Check out the latest version available of my app!");
         appUpdater.setTitleOnUpdateNotAvailable("Update not available");
         appUpdater.setContentOnUpdateNotAvailable("No update available. Check for updates again later!");
         appUpdater.setButtonUpdate("Update now");
         appUpdater.setButtonDoNotShowAgain(null);
         appUpdater.setCancelable(false);
+        appUpdater.showAppUpdated(true);
         appUpdater.start();
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppUpdater();
+
+        TextView tvVersion = (TextView) findViewById(R.id.tvAppVersion);
+
+        tvVersion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppUpdater();
+
+            }
+        });
 
         Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
