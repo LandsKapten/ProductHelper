f0c3d9429d9239da006ebd1eb2825f592b40a171
LandsKapten <65979495+LandsKapten@users.noreply.github.com>
2020-05-27 17:44:45 +0200

    Merge branch 'master' of https://github.com/LandsKapten/ProductHelper
    # Conflicts:
    #	app/update.xml

--- app/src/main/java/com/example/mplproducthelperv20/MainActivity.java
+++ app/src/main/java/com/example/mplproducthelperv20/MainActivity.java
@@ -51,10 +51,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppUpdater();
 
         TextView tvVersion = (TextView) findViewById(R.id.tvAppVersion);
-
         tvVersion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
